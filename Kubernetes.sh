28d5b9c486d28a1f6aaaf9b518d034b9d0ff574f
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-08-07 15:40:44 +0530

    Update Kubernetes.sh

--- Kubernetes.sh
+++ Kubernetes.sh
@@ -1,5 +1,10 @@
 # Start a Kubernetes cluster
 gcloud container clusters create webfrontend --zone $MY_ZONE --num-nodes 2
 
-#check your installed version of Kubernetes
+# check your installed version of Kubernetes
 kubectl version
+
+# launch a single instance of the nginx container
+# kubectl create command caused Kubernetes to create a deployment consisting of a single pod containing the nginx container.
+# default number of pods, is 1
+kubectl create deploy nginx --image=nginx:1.17.10

c41d85cb16bef021af9be8587636e125c9b1c191
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-09-09 14:02:59 +0530

    A

--- computeinstance.sh
+++ computeinstance.sh
@@ -1,4 +1,4 @@
 #1 
 # create the privatenet-us-vm instance, run the following command:
 gcloud compute instances create privatenet-us-vm --zone=us-central1-c --machine-type=f1-micro --subnet=privatesubnet-us
-
+gcloud compute instances create privatenet-us-vm --zone=us-central1-c --machine-type=n1-standard-1 --subnet=privatesubnet-us

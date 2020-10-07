4049cb45c803d63be6dcaa25d4ace6c62cfa64c5
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-10-07 09:51:07 +0530

    Instance Template

---
+++ Startup Script.sh
@@ -0,0 +1,13 @@
+# 1
+# Create a startup script to be used by every virtual machine instance. This script sets up the Nginx server upon startup:
+
+cat << EOF > startup.sh
+#! /bin/bash
+apt-get update
+apt-get install -y nginx
+service nginx start
+sed -i -- 's/nginx/Google Cloud Platform - '"\$HOSTNAME"'/' /var/www/html/index.nginx-debian.html
+EOF
+
+
+   

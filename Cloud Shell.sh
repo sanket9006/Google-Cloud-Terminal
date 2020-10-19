8fa4661c983e4518ccf77c7ab04be44790cff9fd
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-10-19 07:26:13 +0530

    Update Cloud Shell.sh

--- Cloud Shell.sh
+++ Cloud Shell.sh
@@ -2,5 +2,8 @@
 # create the privatenet-us-vm instance, run the following command:
 
 
+
+URL&cloudshell=true
+
 https://console.cloud.google.com/bigquery?project=qwiklabs-gcp-02-8caf5e2b62c6&p=qwiklabs-gcp-02-8caf5e2b62c6&d=iotlabdataset&page=dataset&cloudshell=true
 https://console.cloud.google.com/bigquery?project=qwiklabs-gcp-02-8caf5e2b62c6&p=qwiklabs-gcp-02-8caf5e2b62c6&d=iotlabdataset&page=dataset&cloudshell=false

c3238999d78de1816e816d1ed647e7338f6fc128
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-11-04 09:00:38 +0530

    Update Bucket (Cloud Storage Bucket).sh

--- Bucket (Cloud Storage Bucket).sh
+++ Bucket (Cloud Storage Bucket).sh
@@ -2,3 +2,10 @@
 
 export BUCKET=$PROJECT-spinnaker-config
 gsutil mb -c regional -l us-central1 gs://$BUCKET
+
+# Copy from another bucket to cloud shell
+gsutil -m cp -R gs://spls/gsp290/dataflow-python-examples .
+
+# Copy from cloudshell to bucket
+gsutil cp gs://spls/gsp290/data_files/usa_names.csv gs://$PROJECT/data_files/
+gsutil cp gs://spls/gsp290/data_files/head_usa_names.csv gs://$PROJECT/data_files/
